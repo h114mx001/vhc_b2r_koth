4288b46a7fce64966fd5c00505e9382509440f4f
Nemo <truongng201@gmail.com>
2024-05-28 16:39:14 +0700

    [feat]: minor update web challenge

--- sources/vinuni_cloud/main.py
+++ sources/vinuni_cloud/main.py
@@ -1,11 +1,58 @@
 from flask import Flask
-from flask import render_template
-import os 
+from flask import render_template, request, jsonify, send_file
+
+import os
+import logging
+
 app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 # Max image size: 2MB
+app.config['UPLOAD_FOLDER'] =  os.path.join(os.getcwd(), 'uploads') # Save images to the 'uploads' folder
+ALLOW_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOW_EXTENSIONS
+
+def format_size(size):
+    if size < 1024:
+        return f"{size} bytes"
+    elif size < 1024 * 1024:
+        return f"{round(size / 1024, 2)} KB"
+    else:
+        return f"{round(size / (1024 * 1024), 2)} MB"
 
 @app.route('/')
 def home():
+    if request.args.get('file'):
+        try:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], request.args.get('file'))
+            if os.path.exists(filename):
+                if os.access(filename, os.R_OK):
+                    return send_file(filename)
+                return jsonify({"message": "File cannot read"}), 500
+            return jsonify({"message": "File not found"}), 404
+        except:
+            pass
     return render_template("index.html") 
+
+
+@app.route("/files", methods=["GET"])
+def get_all_files():
+    data = []
+    for file in os.listdir(app.config['UPLOAD_FOLDER']):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file)
+        if os.path.isfile(file_path):
+            file_size = os.path.getsize(file_path)
+            file_size = format_size(file_size)
+            file_name = file.split(".")[0]
+            file_type = file.split(".")[1]
+            data.append({
+                "file_name": file_name,
+                "file_type": file_type,
+                "file_size": file_size
+            })
+    return jsonify({"data": data}), 200
+
 
 @app.route('/upload', methods=["POST"])
 def upload_image():
@@ -14,8 +61,11 @@ def upload_image():
     file = request.files['file']
     if file.filename == '':
         return jsonify({'message': 'No selected file'}), 400
-    if file:
-        filename = secure_filename(file.filename)
+    filename = file.filename
+    size = file.content_length
+    if size > app.config['MAX_CONTENT_LENGTH']:
+        return jsonify({'message': 'Your file upload is over 2MB'}), 400
+    if file and allowed_file(filename):
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
         return jsonify({'message': f'File {filename} uploaded successfully'}), 200
